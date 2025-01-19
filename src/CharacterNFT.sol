cf8a0593e47b87aa79948bc3f2baa41af541bfa4
mohitagarwal24 <agarwalmohit545@gmail.com>
2025-01-19 11:01:00 +0530

    style: format token URIs and function signatures for improved readability

--- src/CharacterNFT.sol
+++ src/CharacterNFT.sol
@@ -13,11 +13,21 @@ contract CharacterNFT is ERC721 {
 
     constructor() ERC721("Character", "CH") {
         s_tokenCounter = 0;
-        s_tokenUris["Doraemon"] = "ipfs://bafkreidqknq7bkdy3m44lnb25fabol2htyudyfdllsu2lw3rc2lfti2hey";
-        s_tokenUris["Nobita"] = "ipfs://bafkreiacsq3d57nvk3ufnyps4tcgrqzvnlzrwofzosvnd3cwy7hrjrshzi";
-        s_tokenUris["Shizuka"] = "ipfs://bafkreihisvxvh3rbmlytowcp4zn2rtyoqe6bbfoa5ppr3rlihqqoitou2a";
-        s_tokenUris["Gian"] = "ipfs://bafkreib457ok4juyko2fzcfr2qoooz3m3j56sagvy6btfkk2phurwsn3p4";
-        s_tokenUris["Suneo"] = "ipfs://bafkreibkd4f7kn43bf4fl6anegifafbnz57nh6nepuaxisjwny3kzctqle";
+        s_tokenUris[
+            "Doraemon"
+        ] = "ipfs://bafkreidqknq7bkdy3m44lnb25fabol2htyudyfdllsu2lw3rc2lfti2hey";
+        s_tokenUris[
+            "Nobita"
+        ] = "ipfs://bafkreiacsq3d57nvk3ufnyps4tcgrqzvnlzrwofzosvnd3cwy7hrjrshzi";
+        s_tokenUris[
+            "Shizuka"
+        ] = "ipfs://bafkreihisvxvh3rbmlytowcp4zn2rtyoqe6bbfoa5ppr3rlihqqoitou2a";
+        s_tokenUris[
+            "Gian"
+        ] = "ipfs://bafkreib457ok4juyko2fzcfr2qoooz3m3j56sagvy6btfkk2phurwsn3p4";
+        s_tokenUris[
+            "Suneo"
+        ] = "ipfs://bafkreibkd4f7kn43bf4fl6anegifafbnz57nh6nepuaxisjwny3kzctqle";
     }
 
     function MintNft(string memory charName) public {
@@ -34,11 +44,15 @@ contract CharacterNFT is ERC721 {
         s_tokenIdToUri[tokenId] = getTokenUriByName(charName);
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         return s_tokenIdToUri[tokenId];
     }
 
-    function getTokenUriByName(string memory charName) public view returns (string memory) {
+    function getTokenUriByName(
+        string memory charName
+    ) public view returns (string memory) {
         return s_tokenUris[charName];
     }
 }
